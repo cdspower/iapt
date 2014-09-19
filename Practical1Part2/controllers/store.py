95d7e6e25578dc65b6a770be5b8a588216208585
Christopher Power <christopher.power@gmail.com>
2014-09-19 11:21:38 +0100

    IAPT Code Base
    Initial commit to create project.

---
+++ Practical1Part2/controllers/store.py
@@ -0,0 +1,3 @@
+# @ IAPT: One single controller that on return just gives control to the store/index.html in the view
+def index():
+    return dict()
---
+++ Practical1Part3/controllers/default.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @IAPT: Very simple controller
+
+def index():
+    return dict()
