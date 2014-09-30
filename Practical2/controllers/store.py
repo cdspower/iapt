d6d091660c2a830477dddd8321fb13207737b454
Christopher Power <christopher.power@gmail.com>
2014-09-30 21:52:40 +0100

    Practical 2 Done

--- Practical2/controllers/store.py
+++ Practical2/controllers/store.py
@@ -1,6 +1,10 @@
+#@IAPT: Here the controller is returning a dictionary that contains all of the rows from the query.
 def books():
-    return dict(features = db(db.products.type == 'Book').select())
+    return dict(books = db(db.products.type == 'Book').select())
 
+#@IAPT: So here is a version which is MVC based where we are just returning control to the view
+#This could be more advanced functionality, like committing changes from the user, doing intermediary
+#calculations that the user needs or any number of things in the business logic of the application.
+#It just happens that in this case, the controller doesn't have any business logic to execute.
 def videos():
-#    return dict(features = db(db.products.type == 'Blu-Ray').select())
     return dict()
