7a889cabdfeb05fc44e51cacce71be2fae77995e
Christopher Power <christopher.power@gmail.com>
2014-10-23 11:47:37 +0100

    Practical 3 Part 3.2
    Up to the end of 3.2

--- Practical3/controllers/default.py
+++ Practical3/controllers/default.py
@@ -27,15 +27,25 @@ def search():
     return dict(form=form, results=results)
 
 def addproduct():
-    addform = FORM('Add a New Product',
-                   DIV(LABEL('Product Name:', _for='product_name')),
+    addform = FORM(DIV(LABEL('Product Name:', _for='product_name')),
                    DIV(INPUT(_name='product_name', requires=IS_NOT_EMPTY())),
                    DIV(LABEL('Product Price:', _for='product_price')),
-                   DIV(INPUT(_name='price',requires=IS_NOT_EMPTY())),
+                   DIV(INPUT(_name='product_price',requires=IS_NOT_EMPTY())),
                    DIV(LABEL('Product Type', _for='product_type')),
                    DIV(SELECT('Book','Blu-Ray', value='b',_name='product_type')),
-                   DIV(LABEL('Product Description', _name='product_description')),
+                   DIV(LABEL('Product Description', _for='product_description')),
                    DIV(TEXTAREA(_name='product_description', requires=IS_NOT_EMPTY())),
+                   DIV(LABEL('Product Publisher', _for='product_publisher')),
+                   DIV(INPUT(_name='product_publisher', requires=IS_NOT_EMPTY())),
                    DIV(INPUT(_type='submit')))
 
+    if addform.accepts(request,session):
+        db.products.insert(name=request.vars.product_name,price=request.vars.product_price, type=request.vars.product_type,
+                           description=request.vars.product_description,publisher=request.vars.product_publisher)
+        db.commit
+        response.flash = 'New product added to database.'
+    elif addform.errors:
+        response.flash = 'One or more of your form fields has an error. Please see below for more information'
+    else:
+        response.flash = 'Please complete the form below to add a new product.'
     return dict(addform=addform)
